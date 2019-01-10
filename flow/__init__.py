df57ddeb3c29e41a1258a33dc7f7646da009b6fc
Arthur Sugden <arthur.sugden@gmail.com>
2019-01-10 16:11:53 -0500

    Small changes.

--- flow/__init__.py
+++ flow/__init__.py
@@ -6,6 +6,6 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 # Sub-folders
 from . import classifier, metadata, misc
 # Individual files
-from . import classify2p, config, glm, labels, outfns, _parseargv, paths, trace2p, xday
+from . import classify2p, config, glm, outfns, _parseargv, paths, trace2p, xday
 # Pull in important classes for easy use
 from .metadata.sorters import Date, Run, RunSorter, DateSorter, DatePairSorter
