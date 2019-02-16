db3844210b0e620476bd60eee6ec642ab9fd7493
Arthur Sugden <arthur.sugden@gmail.com>
2019-02-16 11:34:41 -0500

    Updated mixed effects models.

--- flow/misc/math.py
+++ flow/misc/math.py
@@ -25,6 +25,203 @@ def subformula(formula, data):
     return data.loc[:, keys].copy()
 
 
+def _mixed_keys(y, x, random_effects, categorical, continuous_random_effects):
+    """
+    Return the key names for x, y, and random_effects
+    Parameters
+    ----------
+    y
+    x
+    random_effects
+
+    Returns
+    -------
+    list of strs
+        Keys to be extracted from dataframe
+
+    """
+
+    out = [y] + [v for v in x] + [v for v in categorical]
+    res = [v for v in random_effects] + [v for v in continuous_random_effects]
+    out += [vv for v in res for vv in v.split(':') if len(vv) > 0]
+    return out
+
+
+def mixed_effects_model(df, y, x=(), random_effects=(), categorical=(),
+                        continuous_random_effects=(), family='gaussian',
+                        dropzeros=False, nonlinear=False, R=False):
+    """
+    Create and run a mixed-effects general(ized) linear model.
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+    y : str
+        The value in a dataframe to be fit
+    x : tuple of str
+        A tuple of fixed effects to fit
+    random_effects : tuple of str
+        A tuple of random effects to fit
+    family : str {'gaussian', 'gamma'}
+    link : str {'identity', 'log'}
+    dropzeros : bool
+        If true, remove all cases in which y is 0.
+    nonlinear : bool
+        If true, run a generalized linear model
+        rather than a general linear model.
+    R : bool
+        Force the use of the R package
+
+    Returns
+    -------
+    model
+
+    """
+
+    # Sanitize inputs
+    if isinstance(x, basestring):
+        x = (x, )
+    if isinstance(categorical, basestring):
+        categorical = (categorical, )
+    if isinstance(random_effects, basestring):
+        random_effects = (random_effects, )
+
+    # Curate the data to a minimal size
+    df_keys = _mixed_keys(y, x, random_effects, categorical, continuous_random_effects)
+    sub = df.loc[:, df_keys].copy()
+    if dropzeros:
+        sub.replace(0, np.nan, inplace=True)
+    sub.dropna(inplace=True)
+
+    # Convert columns of strings to integer factors
+    for reff in random_effects:
+        reff = reff.split(':')
+        for v in reff:
+            if len(v) > 0:
+                sub[v], _ = pd.factorize(sub[v])
+
+    # Convert categorical variables to strings
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for c in categorical:
+        vals = sub[c].unique()
+        sub[c] = sub[c].replace({v:alpha[i] for i, v in enumerate(vals)})
+
+    # Make formula
+    form_pieces = ([v for v in x] + [v for v in categorical]
+                   + ['(1|%s)'%s for s in random_effects if len(s) > 0]
+                   + ['(%s)'%s for s in continuous_random_effects if len(s) > 0])
+    formula = '%s ~ %s' % (y, ' + '.join(form_pieces))
+    print(formula)
+
+    if len(random_effects) == 0 and len(continuous_random_effects) == 0:
+        y, X = patsy.dmatrices(formula, sub, return_type='dataframe')
+
+        if link.lower() == 'log':
+            linkfn = sm.families.links.log
+        else:
+            linkfn = sm.families.links.identity
+
+        if family.lower() == 'gamma':
+            family = sm.families.Gamma(link=linkfn)
+        elif family.lower() == 'gaussian' or family == 'normal':
+            family = sm.families.Gaussian(link=linkfn)
+        else:
+            family = sm.families.Poisson(link=linkfn)
+
+        model = sm.GLM(y, X, family=family)
+        glm_results = model.fit()
+        print(glm_results.summary2())
+
+    if nonlinear or len(random_effects) > 1 or R:
+        rdf = pandas2ri.py2ri(sub)
+        pandas2ri.activate()
+        base = importr('base')
+        # stats = importr('stats')
+        afex = importr('afex')
+
+        if nonlinear:
+            model = afex.mixed(formula=formula, data=rdf, method='PB', family=family)
+        else:
+            model = afex.mixed(formula=formula, data=rdf)
+
+        print(base.summary(model))
+
+    return model
+
+def icc(data, icc_type='icc2'):
+    """
+    Calculate intraclass correlation coefficient for data within
+        Brain_Data class
+    ICC Formulas are based on:
+    Shrout, P. E., & Fleiss, J. L. (1979). Intraclass correlations: uses in
+    assessing rater reliability. Psychological bulletin, 86(2), 420.
+    icc1:  x_ij = mu + beta_j + w_ij
+    icc2/3:  x_ij = mu + alpha_i + beta_j + (ab)_ij + epsilon_ij
+    Code modifed from nipype algorithms.icc
+    https://github.com/nipy/nipype/blob/master/nipype/algorithms/icc.py
+    Args:
+        icc_type: type of icc to calculate (icc: voxel random effect,
+                icc2: voxel and column random effect, icc3: voxel and
+                column fixed effect)
+    Returns:
+        ICC: intraclass correlation coefficient
+
+    from: https://github.com/cosanlab/nltools/blob/master/nltools/data/brain_data.py
+    """
+
+    Y = data  # Transpose?
+    [n, k] = Y.shape
+
+    # Degrees of Freedom
+    dfc = k - 1
+    dfe = (n - 1)*(k - 1)
+    dfr = n - 1
+
+    # Sum Square Total
+    mean_Y = np.mean(Y)
+    SST = ((Y - mean_Y)**2).sum()
+
+    # create the design matrix for the different levels
+    x = np.kron(np.eye(k), np.ones((n, 1)))  # sessions
+    x0 = np.tile(np.eye(n), (k, 1))  # subjects
+    X = np.hstack([x, x0])
+
+    # Sum Square Error
+    predicted_Y = np.dot(np.dot(np.dot(X, np.linalg.pinv(np.dot(X.T, X))),
+                                X.T), Y.flatten('F'))
+    residuals = Y.flatten('F') - predicted_Y
+    SSE = (residuals**2).sum()
+
+    MSE = SSE/dfe
+
+    # Sum square column effect - between colums
+    SSC = ((np.mean(Y, 0) - mean_Y)**2).sum()*n
+    MSC = SSC/dfc/n
+
+    # Sum Square subject effect - between rows/subjects
+    SSR = SST - SSC - SSE
+    MSR = SSR/dfr
+
+    if icc_type == 'icc1':
+        # ICC(2,1) = (mean square subject - mean square error) /
+        # (mean square subject + (k-1)*mean square error +
+        # k*(mean square columns - mean square error)/n)
+        # ICC = (MSR - MSRW) / (MSR + (k-1) * MSRW)
+        NotImplementedError("This method isn't implemented yet.")
+
+    elif icc_type == 'icc2':
+        # ICC(2,1) = (mean square subject - mean square error) /
+        # (mean square subject + (k-1)*mean square error +
+        # k*(mean square columns - mean square error)/n)
+        ICC = (MSR - MSE)/(MSR + (k - 1)*MSE + k*(MSC - MSE)/n)
+
+    elif icc_type == 'icc3':
+        # ICC(3,1) = (mean square subject - mean square error) /
+        # (mean square subject + (k-1)*mean square error)
+        ICC = (MSR - MSE)/(MSR + (k - 1)*MSE)
+
+    return ICC
+
 def glm(formula, df, family='gaussian', link='identity', dropzeros=True, r=False):
     """
     Apply a GLM using formula to the data in the dataframe data
