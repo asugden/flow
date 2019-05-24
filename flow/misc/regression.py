34cdb7854acef1720b99e81d5c237a070753363b
Jeff Zaremba <jzaremba@protonmail.com>
2019-05-23 23:44:47 -0400

    Python3 fix

--- flow/misc/regression.py
+++ flow/misc/regression.py
@@ -77,11 +77,11 @@ def mixed_effects_model(df, y, x=(), random_effects=(), categorical=(),
     """
 
     # Sanitize inputs
-    if isinstance(x, basestring):
+    if isinstance(x, str):
         x = (x, )
-    if isinstance(categorical, basestring):
+    if isinstance(categorical, str):
         categorical = (categorical, )
-    if isinstance(random_effects, basestring):
+    if isinstance(random_effects, str):
         random_effects = (random_effects, )
 
     # Curate the data to a minimal size
